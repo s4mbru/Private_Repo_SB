a8165c3e2d459fdfdf94efea3796873d67518667
jlee299 <lee.john2999@gmail.com>
2026-03-05 00:15:03 -0600

    Implemented Finch Movements (Closes #1 #4 #6 #7 #8 #9 #10 #13 #14 #15 #16)

--- backend/private.py
+++ backend/private.py
@@ -1,5 +1,52 @@
 from lib.BirdBrain import Finch
+import time
 
 finch = Finch('A')
+
+#makes a line
+finch.setDisplay([0,1,0,1,0,0,1,0,1,0,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0])
+finch.setMove('F',20, 50)
+finch.playNote(60, 0.5)
+finch.setDisplay([1,1,0,1,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0])
+finch.setMove('F',10, 50)
+
+#makes a triangle
+finch.setDisplay([0,1,0,1,0,0,1,0,1,0,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0])
+finch.setMove('F',8, 50)
+finch.setTurn('R',120, 50)
+finch.setMove('F',8, 50)
+finch.setTurn('R',125, 50)
+finch.setMove('F',8, 50)
+finch.playNote(60, 0.5)
+finch.setDisplay([1,1,0,1,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0])
+finch.setMove('F',10, 50)
+
+#makes a circle
+finch.setDisplay([0,1,0,1,0,0,1,0,1,0,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0])
+finch.setMotors(0, 100)
+time.sleep(3)
+finch.stop()
+finch.playNote(60, 0.5)
+finch.setDisplay([1,1,0,1,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0])
+finch.setMove('F',10, 50)
+
+#makes a wavy line
+finch.setDisplay([0,1,0,1,0,0,1,0,1,0,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0])
+finch.setMotors(0,60 )
+time.sleep(1)
+finch.setMotors(60, 0)
+time.sleep(1)
+finch.setMotors(0, 60)
+time.sleep(1)
+finch.setMotors(60,0 )
+time.sleep(1)
+finch.setMotors(0, 60)
+time.sleep(1)
+finch.stop()
+finch.playNote(60, 0.5)
+finch.setDisplay([1,1,0,1,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0])
+
+
+
 
 
